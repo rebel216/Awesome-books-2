0e3172c7011fdf6add5fa5d0bdcf1d7ddb0866dc
rebel216 <neerajbhardwajpkt@gmail.com>
2022-08-02 03:28:03 +0530

    books list functioanlity added

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ class BookList {
 
   addbook(book) {
     this.data.push(book);
+    localStorage.setItem("bookdata", JSON.stringify(this.data));
   }
 
   removeBook(id) {
     const book = document.getElementById(id);
     book.remove();
     this.data = this.data.filter((bookObj) => bookObj.id !== id);
+    localStorage.setItem("bookdata", JSON.stringify(this.data));
   }
 }
 
@@ -39,10 +41,10 @@ function getBook() {
 
 function CreateUIBookList(bookOBJ) {
   const BookListUi = document.getElementById("Book-List");
-  const bookList = document.createElement("div");
-  bookList.classList.add("list");
+  const bookList = document.createElement("li");
+  bookList.classList.add("book-item");
   bookList.setAttribute("id", bookOBJ.id);
-  bookList.innerHTML = `<p>${bookOBJ.Title} <br/> by ${bookOBJ.Author}</p>`;
+  bookList.innerHTML = `${bookOBJ.Title}by ${bookOBJ.Author}`;
   const deleteBook = document.createElement("button");
   deleteBook.innerHTML = "Remove";
   deleteBook.addEventListener("click", () => booklist.removeBook(bookOBJ.id));
@@ -54,5 +56,83 @@ AddBooks.addEventListener("submit", (e) => {
   const newbook = getBook();
   booklist.addbook(newbook);
   CreateUIBookList(newbook);
+  document.getElementById("bookAuthor").value = "";
+  document.getElementById("bookTitle").value = "";
+  const BookList = document.getElementById("Bookcollection");
+  const Form = document.getElementById("addBooks");
+  const Contact = document.getElementById("Contactinfo");
+  const heading = document.getElementById("title");
+  const linkAllBooks = document.getElementById("link");
+  const linkAddBook = document.getElementById("newBook");
+  const linkContact = document.getElementById("Contact");
+  const BookListheading = document.getElementById("allawesomebooks");
+  BookList.style.display = "grid";
+  BookListheading.style.display = "grid";
+  linkAllBooks.style.color = "blue";
+  linkAddBook.style.color = "black";
+  linkContact.style.color = "black";
+  Form.style.display = "none";
+  Contact.style.display = "none";
+  heading.innerHTML = "All Awesome Books";
   e.preventDefault();
 });
+
+window.onload = () => {
+  booklist.data = JSON.parse(localStorage.getItem("bookdata" || "[]"));
+  if (booklist.data === null) {
+    booklist.data = [];
+    return;
+  }
+
+  booklist.data.forEach((book) => CreateUIBookList(book));
+};
+
+// eslint-disable-next-line no-unused-vars
+function display(section) {
+  const BookList = document.getElementById("Bookcollection");
+  const BookListheading = document.getElementById("allawesomebooks");
+  const Form = document.getElementById("addBooks");
+  const Contact = document.getElementById("Contactinfo");
+  const heading = document.getElementById("title");
+  const linkAllBooks = document.getElementById("link");
+  const linkAddBook = document.getElementById("newBook");
+  const linkContact = document.getElementById("Contact");
+
+  switch (section) {
+    case "Bookcollection":
+      BookList.style.display = "grid";
+      BookListheading.style.display = "grid";
+      linkAllBooks.style.color = "blue";
+      linkAddBook.style.color = "black";
+      linkContact.style.color = "black";
+      Form.style.display = "none";
+      Contact.style.display = "none";
+      heading.innerHTML = "All Awesome Books";
+      break;
+
+    case "addBooks":
+      BookList.style.display = "none";
+      BookListheading.style.display = "none";
+      Form.style.display = "flex";
+      linkAllBooks.style.color = "black";
+      linkAddBook.style.color = "blue";
+      linkContact.style.color = "black";
+      Contact.style.display = "none";
+      heading.innerHTML = "Add a New Book";
+      break;
+
+    case "Contactinfo":
+      BookList.style.display = "none";
+      BookListheading.style.display = "none";
+      Form.style.display = "none";
+      linkAllBooks.style.color = "black";
+      linkAddBook.style.color = "black";
+      linkContact.style.color = "blue";
+      Contact.style.display = "block";
+      heading.innerHTML = "Contact Information";
+      break;
+
+    default:
+      break;
+  }
+}
